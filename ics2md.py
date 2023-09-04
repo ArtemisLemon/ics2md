a828976a495432a3aff597ea73d36fb9084ebebf
Matthew Coyle <matthew.coyle@imt-atlantique.fr>
2023-09-04 15:34:27 +0200

    added ics load func, and completed YamlHead

--- ics2md.py
+++ ics2md.py
@@ -1,7 +1,7 @@
 import icalendar
 
-def YamlHead(data):
-    pass
+def YamlHead(title):
+    out = "---\nlayout : post\n title : {}\n---".format(title)
 
 def write_md(data):
     pass
@@ -10,5 +10,20 @@ def write_md(data):
         file.write(YamlHead(data['yamlhead']))
 
 
-def load_ics(icsurl):
-    pass
+def load_ics():
+    file = open("NaomodWeeklySeminars", 'rb')
+    ical = icalendar.Calendar.from_ical(file.read())
+    out = []
+    for component in ical.walk():
+        if component.name == "VEVENT":
+            out.append(component)
+            print(component.get("name"))
+            print(component.get("description"))
+            print(component.get("organizer"))
+            print(component.get("location"))
+            print(component.decoded("dtstart"))
+            print(component.decoded("dtend"))
+            print("-----------------")
+    file.close()
+
+load_ics()
